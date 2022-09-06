37e61f9f6915f48dca20364f3409c00a8bdfd1e9
jacky32 <patrki@atlas.cz>
2022-09-06 18:27:11 +0200

    Add buildHeader function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,26 @@ import restaurantImage from "./Restaurant_image.jpg";
 
 const content = document.getElementById("content");
 
-const restImg = new Image();
-restImg.src = restaurantImage;
+// const restImg = new Image();
+// restImg.src = restaurantImage;
 
-content.appendChild(restImg);
+// content.appendChild(restImg);
+
+const buildHeader = () => {
+  const header = document.createElement("header");
+  const homeButton = document.createElement("div");
+  const menuButton = document.createElement("div");
+  const contactButton = document.createElement("div");
+
+  homeButton.id = "home";
+  menuButton.id = "menu";
+  contactButton.id = "contact";
+
+  homeButton.textContent = "Home";
+  menuButton.textContent = "Menu";
+  contactButton.textContent = "Contact";
+
+  content.appendChild(header);
+};
+
+buildHeader();
